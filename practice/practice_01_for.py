dbae5f239bef31758d8982ca9482a367010881ef
Gangddonggae <143775289+Gangddonggae@users.noreply.github.com>
2023-10-06 14:40:23 +0900

    Add files via upload

--- practice/practice_01_for.py
+++ practice/practice_01_for.py
@@ -22,6 +22,21 @@ print(round(result, 2))  #평균값
 
 # 문제4) list b에서 최소값 찾기
 b = [22,1,4,7,98]
-for i in b:
+num_min = b[0] #22
+for x in b:
+    if x < num_min:
+        num_min = x
+print(f"최소값: {num_min}")
+#오름차순 만들기
 
-print(num_min) # 1 출력
+#문제 5) list c의 최소값, 최대값 찾기
+c = [2,5,7,1,8]
+num_min = c[0]
+num_max = c[0]
+for x in c:
+    if x < num_min:
+        num_min = x
+    if x > num_max:
+        num_max = x
+print(f"최소값 {num_min}")
+print(f"최대값 {num_max}")
