8f48a88e73c1ba5806c444ab12a5f0f3e89b197a
Gangddonggae <143775289+Gangddonggae@users.noreply.github.com>
2023-11-29 17:34:37 +0900

    Add files via upload

---
+++ lecture/python_11_scheduler.py
@@ -0,0 +1,27 @@
+#스케줄러
+# - 정해진 시간에 동작하도록 프로그램을 스케줄링
+# 예) 5초마다 한번씩 동작
+#    특정일(크리스마스)에만 동작
+#    하루에 한번 동작
+
+#개인서버 운영
+
+# apscheduler
+#   1. blocking
+#   2. background
+#apscheduler api 입력시 설명서 api 뒤에입력
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+
+
+def print_now():
+    print(datetime.now())
+
+# 1.스케줄러 생성
+scheduler = BlockingScheduler()
+# 2.스케줄러 등록(일, 주기, 5초)
+scheduler.add_job(print_now, "interval", seconds=5)
+# 3.스케줄러 실행
+scheduler.start()
